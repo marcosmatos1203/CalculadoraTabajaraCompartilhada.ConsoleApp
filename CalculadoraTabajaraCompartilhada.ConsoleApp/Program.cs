aed2efe60c869cd63ce8939f802966f18fac1722
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-07-30 14:45:49 -0300

    Implementado requisito 3 - subtração

--- CalculadoraTabajaraCompartilhada.ConsoleApp/Program.cs
+++ CalculadoraTabajaraCompartilhada.ConsoleApp/Program.cs
@@ -18,18 +18,23 @@ namespace CalculadoraTabajaraCompartilhada.ConsoleApp
         //marcos
         #endregion
 
-       
+        #region Requisito Funcional 03 [OK]
+        //Nossa calculadora deve ter a possibilidade fazer várias operações de soma e de subtração
+        //Rafa
+        #endregion
+
         static void Main(string[] args)
         {
             string opcao = "";
             while (true)
             {
                 //1.5.1 app 1 verão 5 correção de bug 1
-                Console.WriteLine("Calculador 1.5.1");
+                Console.WriteLine("Calculadora 1.3");
                 Console.WriteLine("Digite 1 para somar");
+                Console.WriteLine("Digite 2 para subtrair");
                 Console.WriteLine("Digite S para sair");
                 opcao = Console.ReadLine();
-                if (opcao != "1" && opcao != "S")
+                if (opcao != "1" && opcao != "S" && opcao != "s" && opcao != "2")
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Opção inválida Tente novamente");
@@ -40,15 +45,23 @@ namespace CalculadoraTabajaraCompartilhada.ConsoleApp
                 }
                 double segundoNumero, primeiroNumero;
 
-                Console.WriteLine("Digite o primeiro número");
+                Console.Write("Digite o primeiro número: ");
 
                 double.TryParse(Console.ReadLine(), out primeiroNumero);
 
-                Console.WriteLine("Digite o segundo número");
+                Console.Write("Digite o segundo número: ");
 
                 double.TryParse(Console.ReadLine(), out segundoNumero);
                 double resultado = 0;
-                resultado = primeiroNumero + segundoNumero;
+                switch (opcao)
+                {
+                    case "1": resultado = primeiroNumero + segundoNumero; break;
+
+                    case "2": resultado = primeiroNumero - segundoNumero; break;
+
+                    default:
+                        break;
+                }
                 Console.WriteLine("Resultado: " + resultado);
                 Console.ReadLine();
                 Console.Clear();
